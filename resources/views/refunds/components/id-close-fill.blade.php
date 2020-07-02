b2599f32501c0df24922f01135ed2c510f2ce01a
Isaac <legato.isaac@gmail.com>
2020-07-02 04:58:51 -0500

    QOL

--- resources/views/refunds/components/id-close-fill.blade.php
+++ resources/views/refunds/components/id-close-fill.blade.php
@@ -1,5 +1,5 @@
 @isset($orden)
-<div class="input-group{{ $errors->has('id') ? ' has-danger' : '' }}" >
+<div class="input-group{{ $errors->has('id') ? ' has-danger' : '' }}" hidden>
 <input type="text" name="id" class="form-control{{ $errors->has('id') ? ' is-invalid' : '' }}" placeholder="{{ __('id') }}" value="{{$orden->id}}" readonly>
     @include('alerts.feedback', ['field' => 'id'])
 </div>
